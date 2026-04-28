948c27067af39d784400d0b435f84a50b34fb673
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2026-04-28 12:41:12 +0000

    Implement GET endpoint for TikTok authentication redirect

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,11 @@
+export async function GET() {
+  const client_key = process.env.TIKTOK_CLIENT_KEY!
+  const redirect_uri = process.env.TIKTOK_REDIRECT_URI!
+
+  const scope = "user.info.basic"
+  const response_type = "code"
+
+  const url = `https://www.tiktok.com/v2/auth/authorize/?client_key=${client_key}&response_type=${response_type}&scope=${scope}&redirect_uri=${redirect_uri}`
+
+  return Response.redirect(url)
+}
